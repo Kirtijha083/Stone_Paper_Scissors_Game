9abf5cde76b963dbbefe3e0df22285b5aef5153d
KirtiJha <kirti.jha2025@gmail.com>
2025-03-29 12:21:24 +0530

    added more logics

--- app.js
+++ app.js
@@ -3,9 +3,29 @@ let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
 
+const genCompChoice = () => {
+    const option = ["rock", "paper", "scissors"];
+// generate random number 0 to 2..multiply with 3 so the range will be 0 to 2
+// for removing decemal value use --> math.floor
+const randIdx = Math.floor(Math.random() * 3);
+return option[randIdx]; //generate computure choice
+}
+
+const playGame = (userChoice) => {
+    console.log("user choice = ", userChoice);
+// then generate computers choice
+const compChoice = genCompChoice();
+console.log("computer choice = ", compChoice);
+} 
+
 choices.forEach((choice) => {
     // console.log(choice);
     choice.addEventListener("click", () =>{
-        console.log("choice was clicked!");
-    });
-})
+        const userChoice = choice.getAttribute("id");
+        // console.log("choice was clicked!", userChoice);
+        playGame(userChoice);
+    }); 
+});
+
+
+// definig fun for all difffert work is known as moduler way of programming..mean hrr ek kaam ke liye ek function (reusable component.....jn jarurat ho call kr lo)
